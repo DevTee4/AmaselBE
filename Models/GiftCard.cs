2bbd42c55cd7c37f01f5ec797e8b8511b1eb02fc
DevTee4 <toyinayodele2012@gmail.com>
2023-01-10 20:11:02 +0100

    Updating models

---
+++ Models/GiftCard.cs
@@ -0,0 +1,15 @@
+namespace AmaselBE.Model
+{
+    public class GiftCard : BaseModel
+    {
+        public decimal Amount { get; set; }
+        public string MailFrom { get; set; }
+        public string MailTo { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset ValidFrom { get; set; }
+        public string PaymentInfo  { get; set; }
+        public string Key { get; set; }
+        public string FromName { get; set; }
+        public string ToName { get; set; }
+    }
+}
